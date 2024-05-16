c9d40168aa7fe1c24fdf2aec1cf8f67246772505
ProudfootDK <56972541+ProudfootDK@users.noreply.github.com>
2024-05-16 02:41:55 -0400

    init commit

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func serveIndex(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(w, "not found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "not found", http.StatusNotFound)
+		return
+	}
+
+	http.ServeFile(w, r, "index.html")
+}
+
+func main() {
+	fmt.Println("hello world")
+
+	hub := NewHub()
+	go hub.Run()
+
+	http.HandleFunc("/", serveIndex)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWS(hub, w, r)
+	})
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
